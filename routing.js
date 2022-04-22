a49c000c0d7611311f37db467e9edd0de9777237
seyyidenurunal <95251609+seyyidenurunal@users.noreply.github.com>
2022-04-22 16:39:34 +0300

    Routing ? * +

--- routing.js
+++ routing.js
@@ -5,11 +5,11 @@ const app = express();
 ? => Zorunlu olmayan  (ilet?isim -- ileisim de yazılsa kabul.) ya da (ile(ti)sim -- ilesim yazarsa kabul)
                                                                     ama parantez içine alınanların hiçbiri olmaması lazım.
 * => Yerine Herhangi bir ifade gelebilir. (ileti*sim -- iletiKSNVSJKNSsim de yazılsa çalışır.)
-+ => Soldaki ifadenin aynısı olmalı.
++ => Soldaki ifadenin aynısını yazmaya devam edildiğinde sorun olmuyor.olmalı.
 
 */
 
-app.get('/ileti+sim', (req,res) => { // Yni eğer url'e iletiisim de yazılmış olsa hata vermeden çalışır.
+app.get('/ileti+sim', (req,res) => { // Yni eğer url'e iletiiiiiiiiiiiisim de yazılmış olsa hata vermeden çalışır.
     res.send("Hello Express");
     console.log("Test");
 });
