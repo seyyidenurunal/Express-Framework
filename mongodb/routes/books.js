5281b02d4207bd397e0680042e2cd9bc14f0d82f
seyyidenurunal <95251609+seyyidenurunal@users.noreply.github.com>
2022-04-25 23:38:52 +0300

    MongoDB

--- mongodb/routes/books.js
+++ mongodb/routes/books.js
@@ -7,7 +7,7 @@ var Book = require('../models/Book');
 
 router.post('/new', function(req, res, next) {
     var book = new Book({
-        title : 'Yeni Dünyanın Cesur İnsanı',
+        title : 'İnsan ne ile yaşar',
         published : true,
         comments :[
             {message : "Harika bir kitap."},
@@ -31,8 +31,15 @@ router.post('/new', function(req, res, next) {
 router.get('/search', (req,res) => { //Sorgu yapmayı sağlar.
     Book.find({published : true, title : "Yeni"}, 'title comments' , (err,data) => { //2. parametre olarak girdiğimiz comment, bulunan verinin yalnızca comment kısmını getirmeyi sağlar. 
         res.json(data);                                                              //Birden fazla arama yapmak için virgülle devam ederiz.
-    });                                                                              //Boş süslü paarantez gönderdiğimizde ise tüm kayıtları getirir.
+    });
+});
+
+router.get('/searchOne', (req, res) => {
+    Book.findOne({published : true}, (err,data) => { //İlk kayıtla uyuştuğu an cevap döner.
+            res.json(data);
+    });
 });
 
 module.exports = router;
+
 
