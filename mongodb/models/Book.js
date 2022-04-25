daeed5c68a61695f7bb774f6b79f67a8ad7fbbf4
seyyidenurunal <95251609+seyyidenurunal@users.noreply.github.com>
2022-04-25 22:17:53 +0300

    Veri oluşturma

--- mongodb/models/Book.js
+++ mongodb/models/Book.js
@@ -2,10 +2,17 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var BookSchema = new Schema({
-    title: String
+    title: String,
+    published: Boolean,
+    comments : [{message : String}],
+    meta : {
+        votes : Number,
+        favs : Number
+    }
 });
 
 module.exports = mongoose.model('Book', BookSchema);
+
 
 
 
