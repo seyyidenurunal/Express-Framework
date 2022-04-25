2462423954deb25058705f9112bebde68a5c5ada
seyyidenurunal <95251609+seyyidenurunal@users.noreply.github.com>
2022-04-25 22:33:07 +0300

    MongoDB

--- mongodb/models/Book.js
+++ mongodb/models/Book.js
@@ -2,16 +2,31 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var BookSchema = new Schema({
-    title: String,
-    published: Boolean,
+    title: {
+        type : String,
+        required : true, // Bu alanın zorunlu olarak doldurulması gerektiğini ifade eder.
+        unique : true, // Bu alanın eşsiz olduğu, yani aynı title da bir kitap daha eklenemeyeceği anlamına gelir.
+    },
     comments : [{message : String}],
     meta : {
         votes : Number,
         favs : Number
-    }
+    },
+    published: {
+        type : Boolean, //Normalde değeri bu şekilde süslü parantez gerekmeden hemen yanına yazabilriz.
+        default : false, //Ancak eğer bu şekilde bir varsayılan değer atanacaksa, o zaman süslü parantez ile yazabliliriz. 
+    },
+    publishedAt :{ //Yayın tarihi
+        type : Date,
+        default : Date.now, //Tarih girilmediğinde varsayılan olarak bugünün tarihini alır.
+    } 
 });
 
 module.exports = mongoose.model('Book', BookSchema);
+
+
+
+
 
 
 
