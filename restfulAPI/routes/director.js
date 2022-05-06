daf29fad6bb03663e1591427a23b961ebd77cb90
seyyidenurunal <95251609+seyyidenurunal@users.noreply.github.com>
2022-05-06 13:44:15 +0300

    retfulAPI

--- restfulAPI/routes/director.js
+++ restfulAPI/routes/director.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { populate } = require('../Models/Director');
 const router = express.Router();
 
 const Director = require('../Models/Director');
@@ -14,4 +15,29 @@ router.post('/', (req, res, next) => {
   })
 });
 
+router.delete('/:director_id', (req,res,next) => {  //veri silme
+  const promise = Director.findByIdAndRemove(req.params.director_id);
+
+ promise.then((director) => {
+   res.json(director);
+ }).catch((err) => {
+   next({message : 'The movie was not found.'});
+   
+ });
+}); 
+
+router.get('/:director_id', (req,res,next) => {  // id bazlı arama için
+ 
+  const promise = Director.findById(req.params.director_id);
+
+ promise.then((director) => {
+  
+   res.json(director);
+ }).catch((err) => {
+   next({message : 'The movie was not found.'});
+   
+ });
+});
+
 module.exports = router;
+
