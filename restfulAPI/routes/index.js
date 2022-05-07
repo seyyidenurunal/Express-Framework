2b77c356b83c69563c9b35b1f13144756efdb9dc
seyyidenurunal <95251609+seyyidenurunal@users.noreply.github.com>
2022-05-07 12:52:01 +0300

    RestfulAPI

--- restfulAPI/routes/index.js
+++ restfulAPI/routes/index.js
@@ -1,27 +1,28 @@
 const express = require('express');
 const router = express.Router();
-
+const bcrypt = require('bcryptjs');
 const User = require('../Models/Users');
 
-router.get('/', (req, res, next) => {
-  res.render({ title: 'Express' });
-});
-
-router.post('/', (req, res, next) => {
+router.post('/register', (req, res, next) => {
   const {username, password} = req.body;
-
-  const user = new User({ 
-    username,
-    password
+ 
+  bcrypt.hash(password, 10) //Şifreyi, Db'e şifreleyerek gönderir.
+  .then((hash) => {
+      const user = new User({ 
+        username,
+        password : hash
+    });
+  
+  
+    const promise = user.save();
+  
+    promise.then((data) => {
+      res.json(data);
+    }).catch((err) => {
+      res.json(err);
+    })
   });
 
-  const promise = user.save();
-
-  promise.then((data) => {
-    res.json(data);
-  }).catch((err) => {
-    res.json(err);
-  });
 });
 
 module.exports = router;
