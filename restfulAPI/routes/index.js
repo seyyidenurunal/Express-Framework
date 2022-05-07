28e0e81fe2c7e0dd504262d367ba963d3d1f688c
seyyidenurunal <95251609+seyyidenurunal@users.noreply.github.com>
2022-05-07 13:39:31 +0300

    RestfulAPI

--- restfulAPI/routes/index.js
+++ restfulAPI/routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../Models/Users');
 
 router.post('/register', (req, res, next) => {
   const {username, password} = req.body;
  
-  bcrypt.hash(password, 10) //Şifreyi, Db'e şifreleyerek gönderir.
+  bcrypt.hash(password, 10)  //Compare metodu ile de şifre çözülür.
   .then((hash) => {
       const user = new User({ 
         username,
@@ -23,6 +24,45 @@ router.post('/register', (req, res, next) => {
     })
   });
 
+});
+
+
+router.post('/authenticate', (req,res) => {
+  const {username, password} = req.body;
+  User.findOne({ 
+    username 
+  }, (err, user) => {
+    if (err)
+      throw err;
+    if (!user){
+      res.json({
+        status : false,
+        message : 'User not found',
+      });
+    }else{
+      bcrypt.compare(password, user.password).then((result) => {
+        if (!result){
+          res.json({
+            status : false, 
+            message : 'Wrong password.',
+          });
+        }else{
+          const payload = {
+            username
+          };
+          const token = jwt.sign(payload, req.app.get('api_secret_key'), {
+            expiresIn: 720, // 12 saat
+          });
+          
+          res.json({
+            status : true,
+            token
+          });
+        }
+      });
+    }
+
+  })
 });
 
 module.exports = router;
